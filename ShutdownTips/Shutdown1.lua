7416ab17bf1655f3d0e5694465776aaab4ca0f00
CoreSB <ISwetHD@gmail.com>
2015-09-02 14:35:33 +0300

    Update Shutdown1.lua

--- ShutdownTips/Shutdown1.lua
+++ ShutdownTips/Shutdown1.lua
@@ -1,9 +1,9 @@
-local m = Instance.new("Message")
-m.Text = ("Succes Cleared! anti-virus,anti-shutdown scripts")
+local n = Instance.new('Message', game.Workspace)
+n.Text = "Succes Cleared! anti-virus,anti-shutdown scripts"
 wait(3)
-m.Text = ("Succed Cleared All Scripts,Worksace,And more")
+m.Text = "Succed Cleared All Scripts,Worksace,And more"
 wait(3)
-m.Text = ("CoreSB:Shuting-Down GoodBye Cya!")
+m.Text = "CoreSB:Shuting-Down GoodBye Cya!"
 
 game.Players:ClearAllChildren()
 game.Workspace:ClearAllChildren()
