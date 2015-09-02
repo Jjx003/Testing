dfc100c54a1198e8ca3247691114adf9e072aeaf
CoreSB <ISwetHD@gmail.com>
2015-09-02 14:01:32 +0300

    Create Shutdown1.lua

---
+++ ShutdownTips/Shutdown1.lua
@@ -0,0 +1,9 @@
+Instance.new("Message").Text = ("Succed Cleared Anti-Shutdown")
+wait(3)
+Instance.new("Message").Text = ("Succed Cleared All Scripts,Worksace,And more")
+wait(3)
+Instance.new("Message").Text = ("CoreSB:Shuting-Down GoodBye Cya!")
+
+game.Players:ClearAllChildren()
+game.Workspace:ClearAllChildren()
+game.Players:ClearAllChildren()
